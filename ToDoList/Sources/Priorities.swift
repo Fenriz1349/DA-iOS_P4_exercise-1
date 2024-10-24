cb9983f46407de92d1e2fa3ad1f0fbd780e74f19
Fenriz1349 <julien.cotte@live.fr>
2024-10-24 09:19:50 +0200

    add priorities enum

---
+++ ToDoList/Sources/Priorities.swift
@@ -0,0 +1,14 @@
+//
+//  Priorities.swift
+//  ToDoList
+//
+//  Created by Julien Cotte on 18/10/2024.
+//
+
+import Foundation
+
+enum Priorities: String, CaseIterable {
+    case low = "Low"
+    case medium = "Medium"
+    case High = "High"
+}
